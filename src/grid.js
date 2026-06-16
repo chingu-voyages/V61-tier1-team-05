ae4601236a99abd1778114129956168c609a21d2
martiNik2 <anas88893142@gmail.com>
2026-06-16 20:29:40 +0300

    add word grid

---
+++ src/grid.js
@@ -0,0 +1,13 @@
+for (let i=0;i<6;i++){
+    const rowDiv=document.createElement("div");
+    rowDiv.id=`row_${i}`;
+    rowDiv.className="grid-row";
+    for (let j=0;j<5;j++){
+        const squareDiv=document.createElement("div");
+        squareDiv.id=`square_${i}_${j}`;
+        squareDiv.className="grid-square";
+        rowDiv.appendChild(squareDiv);
+    }
+    document.body.appendChild(rowDiv);
+}
+
